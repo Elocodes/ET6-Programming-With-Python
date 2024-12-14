86abf944588dcfe8476944e5d15153b6e65df13d
Elocodes <elochukwuodoeze@gmail.com>
2024-12-14 15:01:01 -0500

    renamed file names to match the function. renamed unittests properly

--- 3_documenting_and_testing/exercises/tests/test_sum_numbers.py
+++ 3_documenting_and_testing/exercises/tests/test_sum_numbers.py
@@ -1,34 +1,34 @@
 import unittest
 
-from ..mystery_1 import sum_of_numbers
+from ..sum_numbers import sum_of_numbers
 
-class TestMystery1(unittest.TestCase):
+class TestSumNumbers(unittest.TestCase):
     """Test the sum_of_numbers function"""
 
-    def test_0(self):
+    def test_if_arg_is_int(self):
         """it should throw an error if non integer is passed as argument"""
         with self.assertRaises(AssertionError):
             sum_of_numbers("9", 9)
 
-    def test_1(self):
+    def test_positive_numbers(self):
         """it should return a positive when a and b are positive"""
         actual = sum_of_numbers(700, 59)
         expected = 759
         self.assertEqual(actual, expected)
 
-    def test_2(self):
+    def test_negative_and_positive_numbers(self):
         """it should return a negative when the bigger argument has a negative value"""
         actual = sum_of_numbers(-700, 59)
         expected = -641
         self.assertEqual(actual, expected)
 
-    def test_3(self):
+    def test_sum_of_floats(self):
         """it should sum floats"""
         actual = sum_of_numbers(0.78, 2.811)
         expected = 3.591
         self.assertEqual(actual, expected)
 
-    def test_4(self):
+    def test_sum_of_zeros(self):
         """it should sum zeros"""
         actual = sum_of_numbers(0, 0)
         expected = 0

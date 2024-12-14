ed183dd2e22f2b5c66d30aae305b5c65e150b409
Elocodes <elochukwuodoeze@gmail.com>
2024-12-14 16:56:14 -0500

    renamed file to match func

--- 3_documenting_and_testing/exercises/tests/test_append_to_c.py
+++ 3_documenting_and_testing/exercises/tests/test_append_to_c.py
@@ -1,22 +1,22 @@
 import unittest
 
-from ..mystery_6 import append_to_c
+from ..append_to_c import append_to_c
 
 class TestMystery6(unittest.TestCase):
     """Test the append_to_c function"""
 
-    def test_0(self):
+    def test_arg_type(self):
         """it should raise an assertion error if argument is not int"""
         with self.assertRaises(AssertionError):
             append_to_c(2.5, 8)
 
-    def test_1(self):
+    def test_arg_is_zero(self):
         """it evaluates to [] if a is equal to 0"""
         actual = append_to_c(0, 8)
         expected = []
         self.assertEqual(actual, expected)
 
-    def test_2(self):
+    def test_appends_and_increments(self):
         """it appends to c if len of c is less than a"""
         actual = append_to_c(3, 8)
         expected = [8, 9, 10]
